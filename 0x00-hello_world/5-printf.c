827cc7b7893546bc529ab57e0a6aa9469d78c2db
SADIC-22 <ziblimas09@gmail.com>
2022-12-12 14:54:10 -0800

    task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <<stdio.h>
+/**
+ * main - printing using printf
+ *
+ * Return: 0
+ */
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
