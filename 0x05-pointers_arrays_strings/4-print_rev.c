b07e6c25078198d7560ac00a99a5c7e2204ddda3
SADIC-22 <ziblimas09@gmail.com>
2022-12-19 17:11:02 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - printing a string in reverse
+ * @s: the string to reverse print
+ */
+
+void print_rev(char *s)
+{
+	int x, y;
+
+	y = 0
+		while (s[y] != '\0')
+
+		for (x = y - 1; a >= 0; x--)
+		{
+			_putchar(s[x]);
+		}
+			_putchar('\n');
+}
